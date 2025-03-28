3f59e4dbbc406f3c869516bbd19221d89e029aef
BougrNAV <Abid.Bougroug@nav.no>
2025-03-28 17:15:29 +0100

    Oppdatert pga diverse vulnerabilies

--- src/main/java/no/nav/oebs/pdlhendelser/db/repository/PdlLoggRepositoryImpl.java
+++ src/main/java/no/nav/oebs/pdlhendelser/db/repository/PdlLoggRepositoryImpl.java
@@ -18,7 +18,7 @@ public class PdlLoggRepositoryImpl implements PdlLoggRepositoryCustom {
 
 	@Override
 	public void pingKallLogg() {
-		entityManager.createQuery("SELECT n FROM PdlLogg n WHERE id = 0", PdlLogg.class) //
+		entityManager.createQuery("SELECT n FROM PdlLogg n WHERE n.id = 0", PdlLogg.class) //
 				.getResultList();
 	}
 }
